117c46cbea3cd9dee3cc3235dfda8029612f6874
azmi313 <230601110077@student.uin-malang.ac.id>
2026-06-04 13:08:49 +0700

    Prepare for Railway deployment

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()  # Inisialisasi database
-    application.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host='0.0.0.0', port=port, debug=False)
